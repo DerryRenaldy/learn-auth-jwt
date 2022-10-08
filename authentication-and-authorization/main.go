0014ad40ead13c89281d63183c418432c3fdb164
DerryRenaldy <derryrenaldy2000@gmail.com>
2022-10-08 22:49:34 +0700

    adding hashing password

--- authentication-and-authorization/main.go
+++ authentication-and-authorization/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"encoding/base64"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
 
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating bcrypt hash from password: %w", err)
+	}
+
+	return bs, nil
+}
+
+func compareHashPassword(password string, hashPassword []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPassword, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid Password : %w /n", err)
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+
+	pass := 123456789
+	hashedPass, err := hashPassword(string(pass))
+	if err != nil {
+		panic(err)
+	}
+
+	err = compareHashPassword(string(pass), hashedPass)
+	if err != nil {
+		log.Fatalln("not logged in")
+	}
+
+	log.Println("Logged in!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module belajar-auth-golang
 
 go 1.19
+
+require golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
