a70e468871e398acf728948bf36824b7ccd945c2
Derry Renaldy <derryrenaldy2000@gmail.com>
2022-10-10 10:29:20 +0700

    add jwt

--- authentication-and-authorization/main.go
+++ authentication-and-authorization/main.go
@@ -3,9 +3,28 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 	"log"
+	"time"
 )
+
+type UserClaims struct {
+	jwt.RegisteredClaims
+	SessionId int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+
+	if u.SessionId == 0 {
+		return fmt.Errorf("Invalid Session ID")
+	}
+
+	return nil
+}
 
 func hashPassword(password string) ([]byte, error) {
 	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module belajar-auth-golang
 
 go 1.19
 
-require golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
+)
